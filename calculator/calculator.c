16507c09c002407b9e15e04e26fbfccea165208d
Day Vazquez <dayvazquez@protonmail.com>
2022-08-23 19:31:18 +0200

    Create calculator

---
+++ calculator/calculator.c
@@ -0,0 +1,32 @@
+// Create a calculator program that can add, subtract, multiply, and divide two numbers.
+
+#include <stdio.h>
+
+int main() {
+    // Declare variables
+    int num1, num2;
+    char operator;
+
+    // Prompt user for input
+    printf("Enter a number: ");
+    scanf("%d", &num1);
+    printf("Enter an operator: ");
+    scanf(" %c", &operator);
+    printf("Enter another number: ");
+    scanf("%d", &num2);
+
+    // Perform calculation
+    if (operator == '+') {
+        printf("%d + %d = %d", num1, num2, num1 + num2);
+    } else if (operator == '-') {
+        printf("%d - %d = %d", num1, num2, num1 - num2);
+    } else if (operator == '*') {
+        printf("%d * %d = %d", num1, num2, num1 * num2);
+    } else if (operator == '/') {
+        printf("%d / %d = %d", num1, num2, num1 / num2);
+    } else {
+        printf("Invalid operator");
+    }
+
+    return 0;
+}
